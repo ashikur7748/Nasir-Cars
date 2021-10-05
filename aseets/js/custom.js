c493e86db36e8bd28ed3735f0fbd30d250a3cf0a
ashikur7748 <ashikur7748@gmail.com>
2021-10-05 11:32:27 -0700

    Nasir Car Project uploaded

---
+++ aseets/js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.your-slide').slick({
+        prevArrow: '<span class="pre-arrows"><i class="fas fa-chevron-left"></i></span>',
+        nextArrow: '<span class="nex-arrows"><i class="fas fa-chevron-right"></i></span>',
+    });
+  });
